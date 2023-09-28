77973cd0ed8068ff2f7bff0c01bc05aa77aec558
dbajpeyi <deepankar.bajpeyi@ekata.com>
2023-09-28 02:54:25 +0200

    feat: disable touch devices

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ A frontend webapp for tic-tac-toe game. It support a combination of various mode
 If you see something like this in the reducer, don't freak out. It's not actually mutating the state, just hides the immutability underneath.
 
 - The game is deployed [here](https://tic-tac-toe-5da1e.web.app/)
+- Touch devices are disabled, but you can force it with `force-touch-device` in the query string
 
 ## Scripts for development and building
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import "./App.css"
 import { Game } from "./features/game/components/Game"
 
-function isTouchDevice() {
+function isTouchDevice(): boolean {
   return "ontouchstart" in window || navigator.maxTouchPoints > 0
+}
+
+function isSupportedScreen() {
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString)
+  return !isTouchDevice() || urlParams.has("force-touch-device")
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {isTouchDevice() ? <>Touch devices not supported</> : <Game />}
+        {isSupportedScreen() ? <Game /> : <>Touch devices not supported</>}
         <span></span>
       </header>
     </div>
