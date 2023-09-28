7834340381d0c1191ab421d47cc5358bfde4efdc
dbajpeyi <deepankar.bajpeyi@ekata.com>
2023-09-28 02:48:13 +0200

    feat: disable on touch devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css"
 import { Game } from "./features/game/components/Game"
 
+function isTouchDevice() {
+  return "ontouchstart" in window || navigator.maxTouchPoints > 0
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {<Game />}
+        {isTouchDevice() ? <>Touch devices not supported</> : <Game />}
         <span></span>
       </header>
     </div>
